32b4d99c8f8599517fff49d6805e7c41bceb4935
Alan W <awangc31s@gmail.com>
2024-12-28 18:00:11 -0500

    server

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ async function asyncFunction() {
     conn = await pool.getConnection();
     const rows = await conn.query("SELECT 1 as val");
     console.log(rows);
-    const res = await conn.query("INSERT INTO myTable value (?, ?)", [
-      1,
-      "mariadb",
-    ]);
-    console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+    // const res = await conn.query("INSERT INTO myTable value (?, ?)", [
+    // 1,
+    // "mariadb",
+    // ]);
+    // console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
   } catch (err) {
     throw err;
   } finally {
