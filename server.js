bf8828ab29835f680dced1366ca500d133b83954
Alan W <awangc31s@gmail.com>
2024-12-28 17:19:17 -0500

    mariadb

--- server.js
+++ server.js
@@ -1,3 +1,31 @@
+const mariadb = require("mariadb");
+const pool = mariadb.createPool({
+  host: "mydb.com",
+  user: "myUser",
+  password: "myPassword",
+  connectionLimit: 5,
+});
+async function asyncFunction() {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const rows = await conn.query("SELECT 1 as val");
+    console.log(rows);
+    const res = await conn.query("INSERT INTO myTable value (?, ?)", [
+      1,
+      "mariadb",
+    ]);
+    console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+  } catch (err) {
+    throw err;
+  } finally {
+    if (conn) conn.end();
+  }
+}
+asyncFunction().then(() => {
+  pool.end();
+});
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -20,7 +48,7 @@ app.get("/", (req, res) => {
 });
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8079;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
